64e8eac94ad38a9abe8deed108c74d0303eb678a
Rogério Santos <rogersanctus@gmail.com>
2024-05-18 01:45:21 -0300

    Created the Pessoa ViewModel

---
+++ Dominio/ViewModel/PessoaViewModel.cs
@@ -0,0 +1,66 @@
+using SistemaHospedagem.Dominio.Model;
+using SistemaHospedagem.Lib.MVVM.ViewModel;
+
+namespace SistemaHospedagem.Dominio.ViewModel;
+
+public class PessoaViewModel : ViewModelBase
+{
+  private GerentePessoas gerentePessoas;
+
+  public PessoaViewModel(GerentePessoas gerentePessoas)
+  {
+    this.gerentePessoas = gerentePessoas;
+  }
+
+  public void AtribuirNome(Pessoa pessoa, string nome)
+  {
+    try
+    {
+      pessoa.Nome = nome;
+      this.NotificarViews("AtribuirNome:Sucesso");
+    }
+    catch (ArgumentException ex)
+    {
+      this.NotificarViews("AtribuirNome:Erro", ex.Message);
+    }
+  }
+
+  public void AtribuirSobrenome(Pessoa pessoa, string sobrenome)
+  {
+    try
+    {
+      pessoa.Sobrenome = sobrenome;
+      this.NotificarViews("AtribuirSobrenome:Sucesso");
+    }
+    catch (ArgumentException ex)
+    {
+      this.NotificarViews("AtribuirSobrenome:Erro", ex.Message);
+    }
+  }
+
+  public void AtribuirUsuario(Pessoa pessoa, string usuario)
+  {
+    try
+    {
+      pessoa.Usuario = usuario;
+      this.NotificarViews("AtribuirUsuario:Sucesso");
+    }
+    catch (ArgumentException ex)
+    {
+      this.NotificarViews("AtribuirUsuario:Erro", ex.Message);
+    }
+  }
+
+  public void AdicionarPessoa(Pessoa pessoa)
+  {
+    try
+    {
+      this.gerentePessoas.AdicionarPessoa(pessoa);
+      this.NotificarViews("AdicionarPessoa:Sucesso", pessoa.ToString());
+    }
+    catch (InvalidOperationException ex)
+    {
+      this.NotificarViews("AdicionarPessoa:Erro", ex.Message);
+    }
+  }
+}
